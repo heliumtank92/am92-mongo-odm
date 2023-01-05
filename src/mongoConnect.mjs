31eda546716cf289264e53a738cb504ab00fabb0
Ankit Gandhi <ankitgandhi452@gmail.com>
2023-01-05 13:57:01 +0530

    Updated: Service Name handling & Logging

--- src/mongoConnect.mjs
+++ src/mongoConnect.mjs
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose'
-import CONFIG from './CONFIG.mjs'
-import logger from '@am92/api-logger'
+import CONFIG, { SERVICE } from './CONFIG.mjs'
 
 const { CONNECTION_URI, OPTIONS } = CONFIG
 
 const mongoConnect = async () => {
-  logger.trace('[MongoOdm] Establishing MongoDB Connection...')
+  if (process.env.NODE_ENV !== 'production') {
+    console.info(`[${SERVICE} MongoOdm] Mongoose Debug Mode Enabled for Non-Production Mode.`)
+  }
+  console.trace(`[${SERVICE} MongoOdm] Establishing MongoDB Connection...`)
   await mongoose.connect(CONNECTION_URI, OPTIONS)
 }
 
@@ -14,23 +16,23 @@ export default mongoConnect
 mongoose.set('strictQuery', true)
 
 mongoose.connection.on('connected', () => {
-  logger.success('[MongoOdm] MongoDB Connection Established')
+  console.info(`[${SERVICE} MongoOdm] MongoDB Connection Established`)
 })
 
 mongoose.connection.on('reconnected', () => {
-  logger.success('[MongoOdm] MongoDB Connection Re-established')
+  console.info(`[${SERVICE} MongoOdm] MongoDB Connection Re-established`)
 })
 
 mongoose.connection.on('disconnected', () => {
-  logger.error('[MongoOdm] MongoDB Connection Disconnected')
+  console.error(`[${SERVICE} MongoOdm] MongoDB Connection Disconnected`)
 })
 
 mongoose.connection.on('close', () => {
-  logger.trace('[MongoOdm] MongoDB Connection Closed')
+  console.trace(`[${SERVICE} MongoOdm] MongoDB Connection Closed`)
 })
 
 mongoose.connection.on('error', (error) => {
   throw error
 })
 
-if (process.env.NODE_ENV === 'development') { mongoose.set('debug', true) }
+if (process.env.NODE_ENV !== 'production') { mongoose.set('debug', true) }
